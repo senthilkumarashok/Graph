afd76f13e660c56a9b7bf78b518bd6cbad429123
senkumar-magento <senkumar@adobe.com>
2020-07-09 13:01:14 -0700

    udpating the Main class

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ class Main {
 
     public static void main(String[] args) {
         Graph g = new UnDirectedGraph();
-        g.add(2,3);
-        g.add(1,3);
+        g.addEdge(2,3);
+        g.addEdge(1,3);
+        g.addVertex(4);
+        g.addEdge(4,2);
         g.dfs();
     }
 }
